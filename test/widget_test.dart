bcb10ef08df7f522f36710ab48e0a3aa12efd0d6
santiihoyos <santiago.hoyos93@gmail.com>
2022-04-14 14:09:00 +0200

    FIX - android build

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,10 @@ import 'package:flutter_poc/ui/grpc_chat_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_poc/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const BarberBookApp());
+    await tester.pumpWidget(const GrpcChatApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
